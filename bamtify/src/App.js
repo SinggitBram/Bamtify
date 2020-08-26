5f4c6de6f5e99647de27cfb2d7cf0bd9b824156f
Singgit Bramantha <bambadom@gmail.com>
2020-08-27 06:09:16 +0700

    usecontext done

--- bamtify/src/App.js
+++ bamtify/src/App.js
@@ -1,11 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Login from './Login'
+import Player from './Player'
+import { getTokenFromUrl } from './spotify';
+import SpotifyWebApi from "spotify-web-api-js"
+import { useDataLayerValue } from "./DataLayer";
+
+const spotify = new SpotifyWebApi()
 
 function App() {
+  const [token, setToken] = useState(null)
+  const [{user}, dispatch] = useDataLayerValue()
+
+  useEffect(() => {
+    const hash = getTokenFromUrl()
+    window.location.hash = ""
+    const _token = hash.access_token
+
+    if (_token) {
+      setToken(_token)
+      spotify.setAccessToken(_token)
+      // ambil data kita dari spotify 
+      spotify.getMe().then(user => {
+        dispatch({
+          type: 'SET_USER',
+          user: user,
+        })
+      })
+    }
+  }, [])
+console.log(user)
   return (
     <div className="app">
-      <Login />
+      {
+        token ? (
+          // <Player />
+          <Player />
+        ) : (
+            <Login />
+          )
+      }
     </div>
   );
 }
---
+++ bamtify/src/Player.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Player() {
+    return (
+        <div>
+            <h1>welocme</h1>
+        </div>
+    )
+}
+
+export default Player
